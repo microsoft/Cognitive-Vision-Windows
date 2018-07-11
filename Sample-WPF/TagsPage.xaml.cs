ece762993b161302368844881fd643a36bc835ab
Michal Figarski <michal.figarski@gmail.com>
2018-07-11 09:52:10 +0200

    Added parameter with custom api endpoint to all VisionServiceClient constructors. It was only on GUI and some of constructors had trial enpoint and some default.

--- Sample-WPF/TagsPage.xaml.cs
+++ Sample-WPF/TagsPage.xaml.cs
@@ -73,7 +73,7 @@ namespace VisionAPI_WPF_Samples
             //
             // Create Project Oxford Vision API Service client
             //
-            VisionServiceClient VisionServiceClient = new VisionServiceClient(SubscriptionKey, "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0");
+            VisionServiceClient VisionServiceClient = new VisionServiceClient(SubscriptionKey, SubscriptionEndpoint);
             Log("VisionServiceClient is created");
 
             using (Stream imageFileStream = File.OpenRead(imageFilePath))
@@ -105,7 +105,7 @@ namespace VisionAPI_WPF_Samples
             //
             // Create Project Oxford Vision API Service client
             //
-            VisionServiceClient VisionServiceClient = new VisionServiceClient(SubscriptionKey);
+            VisionServiceClient VisionServiceClient = new VisionServiceClient(SubscriptionKey, SubscriptionEndpoint);
             Log("VisionServiceClient is created");
 
             //
